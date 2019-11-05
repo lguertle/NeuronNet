121ccd28176aaf1fb841e34beced13ef00ac7344
Gürtler Laurent <lguertle@INTRANET.EPFL.CH@SVUbuUniv11.intranet.epfl.ch>
2019-11-05 09:46:43 +0100

    remove build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lguertle/myfiles/Programmation/ProgBa3/Ex_Neurons/NeuronNet
-# Build directory: /home/lguertle/myfiles/Programmation/ProgBa3/Ex_Neurons/NeuronNet/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Ex_Neurons "test_neurons")
